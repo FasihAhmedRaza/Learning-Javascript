51f6c0c188901252a8ce61328557a2d3fe149c44
FasihAhmedRaza <thisfasih@gmail.com>
2024-05-17 12:37:03 +0500

    start array

---
+++ new.js
@@ -0,0 +1,23 @@
+'use strict'; //treat all js code as newer version
+
+// const a = 12
+// let b = "fasih@gmail.com"
+// var c = "12312"
+
+//a = 234   not allowed
+// b="Asdas"
+// c= ""
+
+// console.log(b)
+// console.table([b,c])
+
+// const name = new String('Fasih')
+
+// console.log(name)
+// console.log(name[1])
+// console.log(name.length)
+// console.log(name.substring(0,7))
+
+// console.log(name.slice(-8 ,1))
+
+
